2b9b4564b689a9acf5173b82eda20a1cbb21ead3
Masahide YAMASAKI <yamasaki_masahide@cygames.co.jp>
2025-06-22 17:55:08 +0900

    fix: update manifest version to 2025.6.22.1747

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { crx, defineManifest } from "@crxjs/vite-plugin";
 
 const manifest = defineManifest({
-  version: "1.1.0",
-  version_name: "1.1.0",
+  version: "2025.6.22.1747",
   manifest_version: 3,
   name: "__MSG_namefull__",
   short_name: "__MSG_name__",
